bd8b31307d7a43b61fbc379d324383784a2dd61e
FairyPuck <julian.hamboyan@gmail.com>
2019-12-09 09:23:07 +0100

    Build --prod

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MatInputModule, MatProgressSpinnerModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatInputModule,
+        FormsModule,
+        MatProgressSpinnerModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
